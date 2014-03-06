9a810a60fb673027f7b0b56b7b43dffda2d2bc26
Tero Keinänen <kenkku@kenkku.net>
2014-03-06 09:10:46 +0200

    add socket.io

--- app.js
+++ app.js
@@ -33,6 +33,16 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
